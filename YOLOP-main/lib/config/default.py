438df472890f0b5374fa8a451c27a5273fa02962
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2022-12-12 12:57:43 +0100

    Update default.py

--- YOLOP-main/lib/config/default.py
+++ YOLOP-main/lib/config/default.py
@@ -50,10 +50,10 @@ _C.LOSS.LL_IOU_GAIN = 0.2 # lane line iou loss gain
 
 # DATASET related params
 _C.DATASET = CN(new_allowed=True)
-_C.DATASET.DATAROOT = '../data/images'       # the path of images folder
-_C.DATASET.LABELROOT = '../data/det_annotations'      # the path of det_annotations folder
-_C.DATASET.MASKROOT = '../data/da_seg_annontations'                # the path of da_seg_annotations folder
-_C.DATASET.LANEROOT = '../data/ll_seg_anotations'               # the path of ll_seg_annotations folder
+_C.DATASET.DATAROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/images'       # the path of images folder
+_C.DATASET.LABELROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/det_annotations'      # the path of det_annotations folder
+_C.DATASET.MASKROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/da_seg_annontations'                # the path of da_seg_annotations folder
+_C.DATASET.LANEROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/ll_seg_anotations'               # the path of ll_seg_annotations folder
 _C.DATASET.DATASET = 'BddDataset'
 _C.DATASET.TRAIN_SET = 'train'
 _C.DATASET.TEST_SET = 'val'
