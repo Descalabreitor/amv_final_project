d9ae27477580714fa74ea6c1c0e4ee8fff0ebc69
Descalabreitor <90756596+Descalabreitor@users.noreply.github.com>
2022-12-13 16:39:20 +0100

    Update default.py to proposed changes

--- YOLOP-main/lib/config/default.py
+++ YOLOP-main/lib/config/default.py
@@ -10,9 +10,9 @@ _C.WORKERS = 8
 _C.PIN_MEMORY = False
 _C.PRINT_FREQ = 20
 _C.AUTO_RESUME =False       # Resume from the last training interrupt
-_C.NEED_AUTOANCHOR = False      # Re-select the prior anchor(k-means)    When training from scratch (epoch=0), set it to be ture!
+_C.NEED_AUTOANCHOR = True      # Re-select the prior anchor(k-means)    When training from scratch (epoch=0), set it to be ture!
 _C.DEBUG = False
-_C.num_seg_class = 2
+_C.num_seg_class = 2 # O 3 esta por ver
 
 # Cudnn related params
 _C.CUDNN = CN()
@@ -23,18 +23,18 @@ _C.CUDNN.ENABLED = True
 
 # common params for NETWORK
 _C.MODEL = CN(new_allowed=True)
-_C.MODEL.NAME = ''
+_C.MODEL.NAME = 'yolop_for_drivable_area'
 _C.MODEL.STRU_WITHSHARE = False     #add share_block to segbranch
 _C.MODEL.HEADS_NAME = ['']
 _C.MODEL.PRETRAINED = ""
 _C.MODEL.PRETRAINED_DET = ""
-_C.MODEL.IMAGE_SIZE = [640, 640]  # width * height, ex: 192 * 256
+_C.MODEL.IMAGE_SIZE = [640, 640]  # width * height, ex: 192 * 256 Posible cambio
 _C.MODEL.EXTRA = CN(new_allowed=True)
 
 
 # loss params
 _C.LOSS = CN(new_allowed=True)
-_C.LOSS.LOSS_NAME = ''
+_C.LOSS.LOSS_NAME = 'Loss'
 _C.LOSS.MULTI_HEAD_LAMBDA = None
 _C.LOSS.FL_GAMMA = 0.0  # focal loss gamma
 _C.LOSS.CLS_POS_WEIGHT = 1.0  # classification loss positive weights
@@ -50,9 +50,10 @@ _C.LOSS.LL_IOU_GAIN = 0.2 # lane line iou loss gain
 
 # DATASET related params
 _C.DATASET = CN(new_allowed=True)
+# Actualizar los directorios
 _C.DATASET.DATAROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/images'       # the path of images folder
 _C.DATASET.LABELROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/det_annotations'      # the path of det_annotations folder
-_C.DATASET.MASKROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/da_seg_annotations'                # the path of da_seg_annotations folder
+_C.DATASET.MASKROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/Zips/colormaps (1)'                # the path of da_seg_annotations folder
 _C.DATASET.LANEROOT = '/content/drive/MyDrive/AML/Final_proyect/Data/ll_seg_anotations'               # the path of ll_seg_annotations folder
 _C.DATASET.DATASET = 'BddDataset'
 _C.DATASET.TRAIN_SET = 'train'
@@ -90,7 +91,7 @@ _C.TRAIN.GAMMA1 = 0.99
 _C.TRAIN.GAMMA2 = 0.0
 
 _C.TRAIN.BEGIN_EPOCH = 0
-_C.TRAIN.END_EPOCH = 10
+_C.TRAIN.END_EPOCH = 10 # Posible aumento, nº pequeño para su testeo
 
 _C.TRAIN.VAL_FREQ = 1
 _C.TRAIN.BATCH_SIZE_PER_GPU =24
@@ -114,12 +115,12 @@ _C.TRAIN.DET_ONLY = False          # Only train detection task
 
 
 
-_C.TRAIN.PLOT = True                # 
+_C.TRAIN.PLOT = True
 
 # testing
 _C.TEST = CN(new_allowed=True)
 _C.TEST.BATCH_SIZE_PER_GPU = 24
-_C.TEST.MODEL_FILE = ''
+_C.TEST.MODEL_FILE = 'yolop_for_drivable_area' # No se q es esastamente pero el mismo nombre que al modelo
 _C.TEST.SAVE_JSON = False
 _C.TEST.SAVE_TXT = False
 _C.TEST.PLOTS = True
