41acc2808f0f20c307907511e2f188859abf54c5
Kiara Sánchez Cordero <kiarasanchez212@gmail.com>
2022-12-14 20:28:36 +0100

    imports added to data_preprocessing.py

--- YOLOP-main/tools/data_preprocessing.py
+++ YOLOP-main/tools/data_preprocessing.py
@@ -1,3 +1,5 @@
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 from lib.config import cfg
 import torchvision.transforms as transforms
 import lib.dataset as dataset
