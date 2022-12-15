76554024015ea4c6adb0000e06fd35c31566022b
Descalabreitor <90756596+Descalabreitor@users.noreply.github.com>
2022-12-15 20:49:51 +0100

    update_config.py creation

---
+++ YOLOP-main/tools/update_config.py
@@ -0,0 +1,44 @@
+import argparse
+import os, sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+from lib.config import update_config
+from lib.config import cfg
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train Multitask network')
+    # general
+    # parser.add_argument('--cfg',
+    #                     help='experiment configure file name',
+    #                     required=True,
+    #                     type=str)
+
+    # philly
+    parser.add_argument('--modelDir',
+                        help='model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--logDir',
+                        help='log directory',
+                        type=str,
+                        default='runs/')
+    parser.add_argument('--dataDir',
+                        help='data directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--prevModelDir',
+                        help='prev Model directory',
+                        type=str,
+                        default='')
+
+    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
+    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
+    parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = parse_args()
+    update_config(cfg, args)
