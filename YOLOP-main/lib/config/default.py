023f9894de3ca04a10dd90f1b99bb96b0c24be8c
Descalabreitor <90756596+Descalabreitor@users.noreply.github.com>
2022-12-15 18:26:57 +0100

    update version

--- YOLOP-main/lib/config/default.py
+++ YOLOP-main/lib/config/default.py
@@ -50,11 +50,10 @@ _C.LOSS.LL_IOU_GAIN = 0.2 # lane line iou loss gain
 
 # DATASET related params
 _C.DATASET = CN(new_allowed=True)
-# Actualizar los directorios
-_C.DATASET.DATAROOT = '/content/drive/MyDrive/AML/Final_proyect/Project/Data/images'       # the path of images folder
-_C.DATASET.LABELROOT = '/content/drive/MyDrive/AML/Final_proyect/Project/Data/det_annotations'      # the path of det_annotations folder
-_C.DATASET.MASKROOT = '/content/drive/MyDrive/AML/Final_proyect/Project/Data/drivable_area_colormaps' # the path of da_seg_annotations folder
-_C.DATASET.LANEROOT = '/content/drive/MyDrive/AML/Final_proyect/Project/Data/ll_seg_anotations'               # the path of ll_seg_annotations folder
+_C.DATASET.DATAROOT = './Data/images'       # the path of images folder
+_C.DATASET.LABELROOT = './Data/det_annotations'      # the path of det_annotations folder
+_C.DATASET.MASKROOT = './Data/da_annotations' # the path of da_seg_annotations folder
+_C.DATASET.LANEROOT = './Data/ll_seg_anotations'     # the path of ll_seg_annotations folder
 _C.DATASET.DATASET = 'BddDataset'
 _C.DATASET.TRAIN_SET = 'train'
 _C.DATASET.TEST_SET = 'val'
---
+++ YOLOP-main/tools/data_preprocessing.py
@@ -0,0 +1,70 @@
+import os, sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+from lib.config import cfg
+import torchvision.transforms as transforms
+import lib.dataset as dataset
+import json
+from lib.dataset import BddDataset
+import numpy
+import pickle 
+from contextlib import redirect_stdout
+import yaml
+from lib.dataset import AutoDriveDataset
+
+class NumpyArrayEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def main():
+
+
+    print("begin to load data")
+    # Data loading
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+
+    train_dataset = eval('dataset.' + cfg.DATASET.DATASET)(
+        cfg=cfg,
+        is_train=True,
+        inputsize=cfg.MODEL.IMAGE_SIZE,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])
+    )
+
+    
+
+    with open('./lib/dataset/train_dataset_cfg.yml', 'w') as f:
+        with redirect_stdout(f): print(train_dataset.cfg.dump())
+    
+    with open('./lib/dataset/train_dataset_bdd.json', 'w') as fout:
+        json.dump(train_dataset.db , fout, cls=NumpyArrayEncoder)
+
+
+
+    valid_dataset = eval('dataset.' + cfg.DATASET.DATASET)(
+            cfg=cfg,
+            is_train=False,
+            inputsize=cfg.MODEL.IMAGE_SIZE,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                normalize,
+            ])
+        )
+    
+    with open('./lib/dataset/valid_dataset_cfg.yml', 'w') as f:
+        with redirect_stdout(f): print(valid_dataset.cfg.dump())
+    
+    with open('./lib/dataset/valid_dataset_bdd.json', 'w') as fout:
+        json.dump(valid_dataset.db , fout, cls=NumpyArrayEncoder)
+
+
+
+if __name__ == '__main__':
+    main()
